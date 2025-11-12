44acbfd5efaddc9e556eae2a568f7350d05e91da
onyourrndra <raendra.bastian7@smk.belajar.id>
2025-11-12 11:34:52 +0800

    Add project files.

---
+++ SASXIIRPL2_RaendraBastianPutra/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SASXIIRPL2_RaendraBastianPutra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFromCurrency = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.cmbFrom = new System.Windows.Forms.ComboBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(236, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Currency Converter";
+            // 
+            // lblFromCurrency
+            // 
+            this.lblFromCurrency.AutoSize = true;
+            this.lblFromCurrency.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFromCurrency.Location = new System.Drawing.Point(95, 100);
+            this.lblFromCurrency.Name = "lblFromCurrency";
+            this.lblFromCurrency.Size = new System.Drawing.Size(102, 18);
+            this.lblFromCurrency.TabIndex = 1;
+            this.lblFromCurrency.Text = "Origin Amount";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(98, 136);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(145, 20);
+            this.txtAmount.TabIndex = 2;
+            // 
+            // cmbFrom
+            // 
+            this.cmbFrom.FormattingEnabled = true;
+            this.cmbFrom.Location = new System.Drawing.Point(264, 136);
+            this.cmbFrom.Name = "cmbFrom";
+            this.cmbFrom.Size = new System.Drawing.Size(68, 21);
+            this.cmbFrom.TabIndex = 3;
+            this.cmbFrom.SelectedIndexChanged += new System.EventHandler(this.cmbFrom_SelectedIndexChanged);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(376, 100);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(509, 136);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(145, 20);
+            this.txtResult.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(506, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Indonesian Rupiah";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 284);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.cmbFrom);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.lblFromCurrency);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFromCurrency;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.ComboBox cmbFrom;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SASXIIRPL2_RaendraBastianPutra/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace SASXIIRPL2_RaendraBastianPutra
+{
+    public partial class Form1 : Form
+    {
+
+        string connectionString = "server=localhost;user id=root;password=;database=db_konversi;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+   
+            cmbFrom.Items.Add("USD");
+            cmbFrom.Items.Add("JPY");
+            cmbFrom.Items.Add("EUR");
+            cmbFrom.Items.Add("KRW");
+            cmbFrom.Items.Add("SGD");
+
+
+
+            cmbFrom.SelectedIndex = 0;
+        }
+
+        private void cmbFrom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateCurrencyLabels();
+            ConvertCurrency();
+        }
+
+        private void UpdateCurrencyLabels()
+        {
+            lblFromCurrency.Text = GetCurrencyName(cmbFrom.Text);
+        }
+
+        private string GetCurrencyName(string code)
+        {
+            switch (code)
+            {
+                case "USD": return "United States Dollar";
+                case "JPY": return "Japanese Yen";
+                case "EUR": return "Euro";
+                case "KRW": return "South Korean Won";
+                case "SGD": return "Singapore Dollar";
+
+
+                default: return code;
+            }
+        }
+
+        private void ConvertCurrency()
+        {
+            
+         
+
+           
+            if (double.TryParse(txtAmount.Text, out double amount))
+            {
+                string from = cmbFrom.Text;
+
+                double rate = GetRateFromDatabase(from);
+                double result = amount * rate;
+
+                
+                long roundedResult = (long)Math.Round(result);
+
+               
+                txtResult.Text = roundedResult.ToString("N0");
+            }
+            
+        }
+
+
+        private double GetRateFromDatabase(string from)
+        {
+            double rate = 1;
+            string query = "SELECT kurs FROM nilai_tukar WHERE singkatan=@from";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@from", from);
+
+                    object result = cmd.ExecuteScalar();
+                    if (result != null)
+                    {
+                        rate = Convert.ToDouble(result);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nilai tukar tidak ditemukan untuk mata uang ini.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal mengambil nilai tukar: " + ex.Message);
+                }
+            }
+
+            return rate;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            ConvertCurrency();
+
+        }
+    }
+
+}
